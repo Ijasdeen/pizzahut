2832aca1adb346122519ab15d4e36014adb3e931
Ijasdeen <ijasdeen23@gmail.com>
2018-07-14 10:06:01 +0530

    updated

---
+++ checkout.php
@@ -0,0 +1,10 @@
+<?php require_once('includes/header.php')?>
+<div class="container">
+    <div class="row">
+    <div class="col-md-12">
+        
+    </div>
+    </div>
+</div>
+
+<?php require_once('includes/footer.php')?>
---
+++ contact.php
@@ -0,0 +1,65 @@
+<?php require_once('includes/header.php')?>
+<main role="main">
+    <div class="container-fluid">
+    <div class="contactUs-herosection">
+
+        <div class="hero-section">
+
+            <div class="dark-overlay">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+
+                            <div class="hero-sectionContent">
+                                <h3 class="text text-center">CONTACT US, <span>WE LOVE HEARING FROM YOU.</span></h3>
+                            </div>
+                          
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+ 
+    </div>
+    
+       </div>
+       
+<!--       Why tennis pizza hut-->
+      
+      <div class="about-pizzaHut">
+      <div class="continer">
+          <div class="row">
+              <div class="col-md-4">
+                  
+              </div>
+              <div class="col-md-4"></div>
+              <div class="col-md-4"></div>
+          </div>
+      </div>       
+      </div>
+       
+       
+       
+       
+       
+       
+       
+       
+       
+<!--       Map-->
+     <div class="container-fluid">
+         <div class="col-md-12">
+             <div class="map-area">
+            <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="333" id="gmap_canvas" src="https://maps.google.com/maps?q=Batticaloa%20pizza%20hut&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{text-align:right;height:333px;width:100%;}.gmap_canvas {overflow:hidden;background:none!important;height:333px;width:100%;}</style></div>
+        </div>
+         </div>
+     </div>
+
+<div style="height:1800px;"></div>
+    
+    
+</main>
+
+<?php require_once('includes/footer.php')?>
